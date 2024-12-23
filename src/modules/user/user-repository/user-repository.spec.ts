6be70bd0f9f6781b69c23758e5aff18eea02b18e
redlintles <treelands54@gmail.com>
2024-12-23 19:39:41 -0300

    Criados repositório e serviço em user

---
+++ src/modules/user/user-repository/user-repository.spec.ts
@@ -0,0 +1,57 @@
+import { Repository } from "typeorm";
+import { UserRepository } from "./user-repository";
+import { User } from "../entities/user/user";
+import { UserDto } from "../dtos/user-dto/user-dto";
+import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken } from "@nestjs/typeorm";
+
+describe("UserRepository", () => {
+  let userRepository: UserRepository;
+  let repository: Repository<User>;
+
+  const mockUserDto: UserDto = {
+    username: "John Doe",
+    email: "Banana@gmail.com",
+    password: "Banana@123",
+    phone_number: "12345678910",
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        UserRepository,
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
+    }).compile();
+
+    userRepository = module.get<UserRepository>(UserRepository);
+    repository = module.get<Repository<User>>(getRepositoryToken(User));
+  });
+
+  it("should be defined", () => {
+    expect(userRepository).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  describe("createUser", () => {
+    it("Should create and save a user", async () => {
+      const mockUser = new User();
+      mockUser.username = mockUserDto.username;
+      mockUser.email = mockUserDto.email;
+      mockUser.password = mockUserDto.password;
+      mockUser.phone_number = mockUserDto.phone_number;
+
+      jest.spyOn(repository, "create").mockReturnValue(mockUser);
+      jest.spyOn(repository, "save").mockResolvedValue(mockUser);
+
+      const result = await userRepository.createUser(mockUserDto);
+
+      expect(result).toEqual(mockUser);
+      expect(repository.create).toHaveBeenCalledWith(mockUserDto);
+      expect(repository.save).toHaveBeenCalledWith(mockUserDto);
+    });
+  });
+});
---
+++ src/modules/user/user-repository/user-repository.ts
@@ -0,0 +1,19 @@
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "../entities/user/user";
+import { Repository } from "typeorm";
+import { Injectable } from "@nestjs/common";
+import { UserDto } from "../dtos/user-dto/user-dto";
+
+@Injectable()
+export class UserRepository {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async createUser(userDTO: UserDto): Promise<User> {
+    const user = this.userRepository.create(userDTO);
+    await this.userRepository.save(user);
+    return user;
+  }
+}
