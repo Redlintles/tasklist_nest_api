050838e52c96988a7a0fc31c873f466eb06cf804
redlintles <treelands54@gmail.com>
2024-12-27 20:06:35 -0300

    Removidos exception filters desnecessários

--- src/exception-filters/entity-not-found-exception-filter/entity-not-found-exception-filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { EntityNotFoundExceptionFilter } from "./entity-not-found-exception-filter";
-
-describe("EntityNotFoundExceptionFilter", () => {
-  it("should be defined", () => {
-    expect(new EntityNotFoundExceptionFilter()).toBeDefined();
-  });
-});
--- src/exception-filters/unique-constraint-exception-filter/unique-constraint-exception-filter.spec.ts
+++
@@ -1,7 +0,0 @@
-import { UniqueConstraintExceptionFilter } from "./unique-constraint-exception-filter";
-
-describe("UniqueConstraintExceptionFilter", () => {
-  it("should be defined", () => {
-    expect(new UniqueConstraintExceptionFilter()).toBeDefined();
-  });
-});
